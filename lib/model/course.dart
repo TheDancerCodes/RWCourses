76c1b2dc070a1e30dcbb80e2b798519a917acaef
Roger Taracha <taracha@Rogers-MacBook-Pro.local>
2021-07-21 13:23:28 +0300

    Add More Properties: [+] Add properties for artwork, difficulty and contributors.

--- lib/model/course.dart
+++ lib/model/course.dart
@@ -2,12 +2,18 @@ class Course {
   final String courseId;
   final String name;
   final String description;
+  final String artworkUrl;
+  final String difficulty;
+  final String contributors;
 
   // Since all the parameters are marked as final, we need to add a constructor
   Course(
     this.courseId,
     this.name,
     this.description,
+    this.artworkUrl,
+    this.difficulty,
+    this.contributors,
   );
 
   /* Add fromJson() method ~ Takes one parameter which is a Map from String to dynamic
@@ -17,11 +23,14 @@ class Course {
   Course.fromJson(Map<String, dynamic> json)
     : courseId = json["id"],
       name = json["attributes"]["name"],
-      description = json["attributes"]["description_plain_text"];
+      description = json["attributes"]["description_plain_text"],
+      artworkUrl = json["attributes"]["card_artwork_url"],
+      difficulty = json["attributes"]["difficulty"],
+      contributors = json["attributes"]["contributor_string"];
 
       // Add a toString() method inorder to see the response in the app.
       @override
   String toString() {
-    return name;
+    return name + ": " + difficulty;
   }
 }
