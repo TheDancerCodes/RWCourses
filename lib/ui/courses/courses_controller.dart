c797e269d8e2adeb624545944a8fbbbcb1b393a1
Roger Taracha <taracha@Rogers-MacBook-Pro.local>
2021-08-18 20:57:07 +0300

    Dependency Injection: [+] DI is used alongside interfaces to help achieve loose coupling.
    [+] courses_controller.dart ~ We currently have direct coupling between CoursesController and CourseRepository.
    
    [+] courses_page.dart ~ Inject a specific repository type into CoursesController()

--- lib/ui/courses/courses_controller.dart
+++ lib/ui/courses/courses_controller.dart
@@ -1,12 +1,16 @@
 // Controller class to interact with the API
 import 'package:rwcourses/model/course.dart';
-import 'package:rwcourses/repository/course_repository.dart';
 import 'package:rwcourses/repository/repository.dart';
 
 class CoursesController {
 
   // Private repository property
-  final Repository _repository = CourseRepository();
+  final Repository _repository;
+
+  // Constructor to set the repository property value. 
+  // This lets us inject a repository into the controller. The controller is no longer aware of 
+  // a specific type of repository & now it only knows about the repository interface.
+  CoursesController(this._repository);
 
   // Method to fetch courses ~ fetchCourses() method passes off the fetching of courses to the 
   // repository
