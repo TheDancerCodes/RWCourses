0c39c45f081bb8aaaccc597104ce6e0464400b18
TheDancerCodes <roger.taracha@gmail.com>
2022-05-25 14:10:45 +0300

    Lift State Up:
    [+] Use the filter state container and inherited widget you've create to lift filter state up to where it can be seen by both the courses page and the filter page.
    
    [+] filter_page.dart:
        [=] Replace the filterValue property with a filter state
    
        [=] Update the handleRadioValueChanged() method to just call the update filter method on the state object.
    
        [=] To the set the value of the state property, we add a method override in FilterPageState for its didChangeDependencies() method.
    
    [+] courses_page.dart:
        [=] Make similar changes to the filter_page.dart above
    
    [+] main.dart:
        [=] Ensure the FilterStateContainer wraps the material app widget in main.dart
    
        [=] That puts the FilterStateContainer & the InheritedWidget above the other pages in the widget tree.

--- lib/ui/filter/filter_page.dart
+++ lib/ui/filter/filter_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:rwcourses/state/filter_state_container.dart';
 import 'package:rwcourses/strings.dart';
 
 import '../../constants.dart';
@@ -13,35 +13,18 @@ class FilterPage extends StatefulWidget {
 
 class _FilterPageState extends State<FilterPage> {
 
-  // Property to hold the currently selected filter value.
-  int _filterValue = Constants.allFilter;
+  // Instance of FilterState class to hold the currently selected filter value.
+  FilterState state;
 
-  // Add initState to call to loadValue
-  // Calling the new loadValue() function from the initState() override for _FilterPageState
-  // SOLUTION: This lets is retrieve the currently saved filterValue that is in SharedPreferences
+  // To set the value of the state property, add this method override
   @override
-  void initState() {
-    super.initState();
-    _loadValue();
-  }
-
-    // Helper Method - to load the currently saved filter value when the
-  // FilterPage is first opened
-  // NOTE: The call to getInstance is asynchronous, we will use the
-  // async/await keywords
-  void _loadValue() async {
-    final prefs = await SharedPreferences.getInstance();
-
-    // Call setState and set the filterValue for the FilterPage.
-    // We will set it using the current value for the filter that is stored in
-    // SharedPreferences.
-    // SOLUTION: We are calling getInt() on prefs, which is going to return an integer from 
-    // SharedPreferences & we are passing in a key that the sharedPref for the filterValue is going 
-    // to be saved under.
-    setState(() {
-      _filterValue = prefs.getInt(Constants.filterKey) ?? 0;
-    });
-
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    
+    // By setting the state property to the FilterStateContainer value, whenever the inherited widget 
+    // is updated, so long as its updateShouldNotify() returns true, the state property of the FilterPageState 
+    // will be updated as well & the page will be rebuilt.
+    state = FilterStateContainer.of(context);
   }
 
   @override
@@ -56,43 +39,43 @@ class _FilterPageState extends State<FilterPage> {
             children: <Widget>[
               FilterWidget(
                 value: Constants.iosFilter,
-                groupValue: _filterValue,
+                groupValue: state.filterValue,
                 onChanged: _handleRadioValueChange,
                 text: Strings.ios,
               ),
               FilterWidget(
                 value: Constants.androidFilter,
-                groupValue: _filterValue,
+                groupValue: state.filterValue,
                 onChanged: _handleRadioValueChange,
                 text: Strings.android,
               ),
               FilterWidget(
                 value: Constants.flutterFilter,
-                groupValue: _filterValue,
+                groupValue: state.filterValue,
                 onChanged: _handleRadioValueChange,
                 text: Strings.flutter,
               ),
               FilterWidget(
                 value: Constants.sssFilter,
-                groupValue: _filterValue,
+                groupValue: state.filterValue,
                 onChanged: _handleRadioValueChange,
                 text: Strings.sss,
               ),
               FilterWidget(
                 value: Constants.unityFilter,
-                groupValue: _filterValue,
+                groupValue: state.filterValue,
                 onChanged: _handleRadioValueChange,
                 text: Strings.unity,
               ),
               FilterWidget(
                 value: Constants.macosFilter,
-                groupValue: _filterValue,
+                groupValue: state.filterValue,
                 onChanged: _handleRadioValueChange,
                 text: Strings.macos,
               ),
               FilterWidget(
                 value: Constants.allFilter,
-                groupValue: _filterValue,
+                groupValue: state.filterValue,
                 onChanged: _handleRadioValueChange,
                 text: Strings.all,
               ),
@@ -108,17 +91,6 @@ class _FilterPageState extends State<FilterPage> {
   // Save the filter selected by a user using SharedPreferences.
   // TODO: Update to (int? value)
   void _handleRadioValueChange(int value) async {
-
-    // Get the SharedPreferences instance
-    final prefs = await SharedPreferences.getInstance();
-
-    setState(() {  // set the filterValue
-      _filterValue = value ?? 0;
-
-      // Save the currently selected value into SharedPreferences using setInt
-      // SOLUTION: We call setInt() on prefs, we pass in the same key we are going to use to retrieve 
-      // the filter value & we pass in the currently selected filterValue.
-      prefs.setInt(Constants.filterKey, _filterValue);
-    });
+    state.updateFilterValue(value);
   }
 }
